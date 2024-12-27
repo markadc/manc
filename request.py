d8bfc97e4568dc5b009dacd5a7439296b12f42a8
markadc <markadc@126.com>
2024-12-27 16:08:38 +0800

    一些优化

--- request.py
+++ request.py
@@ -9,14 +9,7 @@ from manc.response import Response
 class Request:
     """请求对象"""
 
-    def __init__(
-            self,
-            url: str, method="GET", headers: dict = None, params: dict | str = None,
-            data: dict = None, json: dict = None,
-            proxies: dict = None, timeout: int | float = 5,
-            defer: int | float = 0, retry: int = 2, delay: int | float = 1,
-            **kwargs
-    ):
+    def __init__(self, url: str, method="GET", headers: dict = None, params: dict | str = None, data: dict = None, json: dict = None, proxies: dict = None, timeout=5, retry=2, rest=1, **kwargs):
         self.url = url
         self.method = method
         self.headers = headers
@@ -26,9 +19,8 @@ class Request:
         self.proxies = proxies
         self.timeout = timeout
 
-        self.defer = defer
         self.retry = retry
-        self.delay = delay
+        self.rest = rest
 
         self.kwargs = kwargs
 
@@ -42,9 +34,13 @@ class Request:
             """.format(url, e, times)
         )
 
+    def sleep(self):
+        time.sleep(self.rest)
+
     def do(self):
         if self.method not in ["GET", "POST"]:
             raise ValueError("Method must be GET or POST")
+
         same = dict(headers=self.headers, params=self.params, proxies=self.proxies, timeout=self.timeout, **self.kwargs)
         for i in range(self.retry + 1):
             try:
@@ -54,7 +50,7 @@ class Request:
                     response = requests.post(self.url, data=self.data, json=self.json, **same)
             except Exception as e:
                 self.elog(self.url, e, i + 1)
-                time.sleep(self.delay)
+                self.sleep()
                 if i == self.retry:
                     raise e
             else:
--- spider.py
+++ spider.py
@@ -6,37 +6,43 @@ class BaseSpider:
     plugins: list[SpiderPlugin] = []
 
     def on_request(self, request: Request):
+        """请求之前"""
         for plugin in self.plugins:
             plugin.deal_request(request)
 
     def on_response(self, response: Response):
+        """响应之后"""
         for plugin in self.plugins:
             response = plugin.deal_response(response)
         return response
 
     def add_plugin(self, plugin: SpiderPlugin):
+        """加入扩展"""
         self.plugins.append(plugin)
 
     def add_plugins(self, plugins: list[SpiderPlugin]):
+        """加入多个扩展"""
         for plugin in plugins:
             self.add_plugin(plugin)
 
     def goto(self, url: str, headers: dict = None, params: dict = None, data: dict | str = None, json: dict = None, proxies: dict = None, timeout: int | float = 5, **kwargs):
-        if data is None and json is None:
-            request = Request(url, headers=headers, params=params, proxies=proxies, timeout=timeout, **kwargs)
-        else:
-            request = Request(url, headers=headers, params=params, data=data, json=json, proxies=proxies, timeout=timeout, **kwargs)
-        return self.perform(request)
+        """GET or POST"""
+        same = dict(headers=headers, params=params, proxies=proxies, timeout=timeout, **kwargs)
+        req = Request(url, **same) if data is None and json is None else Request(url, data=data, json=json, **same)
+        return self.perform(req)
 
-    def get(self, url: str, headers: dict = None, params: dict = None, proxies: dict = None, timeout: int | float = 5, **kwargs):
+    def get(self, url: str, headers: dict = None, params: dict = None, proxies: dict = None, timeout=5, **kwargs):
+        """GET"""
         req = Request(url, headers=headers, params=params, proxies=proxies, timeout=timeout, **kwargs)
         return self.perform(req)
 
-    def post(self, url: str, headers: dict = None, params: dict = None, data: dict | str = None, json: dict = None, proxies: dict = None, timeout: int | float = 5, **kwargs):
+    def post(self, url: str, headers: dict = None, params: dict = None, data: dict | str = None, json: dict = None, proxies: dict = None, timeout=5, **kwargs):
+        """POST"""
         req = Request(url, headers=headers, params=params, data=data, json=json, proxies=proxies, timeout=timeout, **kwargs)
         return self.perform(req)
 
     def perform(self, request: Request):
+        """处理请求，返回响应"""
         self.on_request(request)
         response = request.do()
         response = self.on_response(response)
